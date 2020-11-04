f87e18d002ad4cd9b4bb514a6673851338fcf1b8
Grad Marius Beniamin <gradmariusbeniamin@gmail.com>
2020-11-04 22:49:27 +0200

    Update ln.sh

--- ln.sh
+++ ln.sh
@@ -12,11 +12,12 @@ do
         sudo ln -s /home/$me/ftp/server/csgoserver/serverfiles/csgo/$i /var/www/html/fastdl
 done 
 
-if [ ! grep -F "<Directory /var/www/html/fastdl" /etc/apache2/sites-available/000-default.conf ]; then
+if ! grep -F "<Directory /var/www/html/fastdl" /etc/apache2/sites-available/000-default.conf; then
 
-        echo "<Directory /var/www/html/fastdl>" >> /etc/apache2/sites-available/000-default.conf
-        echo "Allow from all" >> /etc/apache2/sites-available/000-default.conf
-        echo "</Directory>" >> /etc/apache2/sites-available/000-default.conf
+        sudo bash -c 'echo "<Directory /var/www/html/fastdl>" >> /etc/apache2/sites-available/000-default.conf'
+        sudo bash -c 'echo "Allow from all" >> /etc/apache2/sites-available/000-default.conf'
+        sudo bash -c 'echo "</Directory>" >> /etc/apache2/sites-available/000-default.conf'
+
         sudo service apache2 restart
 
 fi
